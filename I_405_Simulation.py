ce6d699e9581ec2241e22d6767bc579417080308
guzmad <guzmad@uw.edu>
2018-05-17 20:35:11 -0700

    starting the project

--- I_405_Simulation.py
+++ I_405_Simulation.py
@@ -1,7 +1,21 @@
-# We good
+import numpy as np
+import math
 
-# OH BOI <---- TRAN
+#Constants
+REGULAR = 1
+EXIT = 2
+TOLL = 3
+ON_RAMP = 4
+LINES = 5
+INDEX_REPRESENTING_MILES = 2320
 
-# I'm just trying my best 
 
-#finally
+#The freeway is represented as a 2D array
+freeway = road()
+
+def road():
+    for i in INDEX_REPRESENTING_MILES:
+        #
+    
+
+
