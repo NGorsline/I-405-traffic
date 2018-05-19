5710edafd9be8edb0be9ecab8635c97926952f10
guzmad <guzmad@uw.edu>
2018-05-18 23:19:54 -0700

    Added the on and off ramps to the freeway

--- I_405_Simulation.py
+++ I_405_Simulation.py
@@ -5,15 +5,23 @@ import math
 
 global freeway
 
-#Constants
+#CONSTANTS
+#---------
+#Represents regular lanes in the freeway
 REGULAR = 1
-EXIT = 2
+#Represents the off ramp on the freeway
+OFF_RAMP = 2
+#Represents the toll lane on the freeway
 TOLL = 3
+#Represents the on ramp on the freeway
 ON_RAMP = 4
+#Represents the total lanes on the freeway
 LINES = 5
+#Represents locations of the freeway where a car is not allowed to drive in
 NOT_USED = -1
-MILES = 2320
-
+#Represents the total number of indexes we need to represent 6.8 miles.
+#Each index represent 15 feet.
+MILES = 2320  
 
 attributeList = [0, False, "Car"]
 aList = np.array(attributeList)
@@ -21,7 +29,6 @@ s = (MILES, 4, 3)
 freeway = np.zeros(s, dtype = object)
 
 #The freeway is represented as a 2D array
-
 def initializeRoad():
     global freeway
     freeway[:, 1:3, 0] = REGULAR
@@ -38,7 +45,17 @@ def initializeRoad():
                 freeway[i][j][1] = True
                 freeway[i][j][2] = "C"
 
+# Adds the on and off ramps to the freeway
+def AddingRampsToFreeway():
+    for i in xrange(len(freeway)):
+         #setting the off ramps value to the freeway
+         if (i >= 774 and i <= 844) or (i >= 1547 and i <= 1653) or (i >= 1794 and i <= 1900):
+              freeway[i, 0, 0] = OFF_RAMP
+         #setting the on ramps value to the freeway
+         if (i >= 845 and i <= 951) or (i >= 1654 and i <= 1724) or (i >= 1901 and i <= 2042):
+              freeway[i, 0, 0] = ON_RAMP
 
 initializeRoad()
+AddingRampsToFreeway()
 
 print(freeway)
