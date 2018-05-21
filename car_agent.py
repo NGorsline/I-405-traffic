9811471ad81f4e8cb8f0f6cbe358e7943c2a7946
nathanaelFixx <nathanaelfixx@gmail.com>
2018-05-21 13:02:49 -0700

    car agent

--- car_agent.py
+++ car_agent.py
@@ -3,34 +3,34 @@ a = 3
 print ("man wtf" + str(a))
 #fuck you
 
-class Car:
-    # Constructor 
-    def __init__(self, x, y):
-        # class variables
-        self.curr_location = (x, y)
-        self.finishing_time = None  # will be set when car passes the end of the road
+#class Car:
+#    # Constructor 
+#    def __init__(self, x, y):
+#        # class variables
+#        self.curr_location = (x, y)
+#        self.finishing_time = None  # will be set when car passes the end of the road
 
-    def switch_lane(self):
-        # some shit
+#    def switch_lane(self):
+#        # some shit
 
     
-    def drive(self):
-        # change lane 
-        # move foward (accelerate and decelerate accordingly)
-        # enter toll lane if near it (by a percent)
-        # exit if near exit (by a percent)
-        # stay 
-        return 1
+#    def drive(self):
+#        # change lane 
+#        # move foward (accelerate and decelerate accordingly)
+#        # enter toll lane if near it (by a percent)
+#        # exit if near exit (by a percent)
+#        # stay 
+#        return 1
 
-    def change_lane(self):
-        # potential_space_switch < --- variable
-        # Check if there's an open space next to you (left and right) 
-        #### checking which lane is faster
-        # if yes
-            # 
-    def merge(self):
+#    def change_lane(self):
+#        # potential_space_switch < --- variable
+#        # Check if there's an open space next to you (left and right) 
+#        #### checking which lane is faster
+#        # if yes
+#            # 
+#    def merge(self):
         
-    def enter_toll_lane(self):
+#    def enter_toll_lane(self):
 
     
 
